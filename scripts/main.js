16f7d1f71e303a6bcb67c7e9d0e4830e0afe4d8c
mery vera <meryveramendozamail.com>
2020-08-15 14:57:58 -0500

    hackaton12

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+import dropdown from './dropdown';
+import accordeon from './accordeon';
+import modal from './modal';
+
+console.log(dropdown);
+new dropcdown();
+console.log(accordeon);
+new accordeon();
+console.log(modal);
+new modal();
